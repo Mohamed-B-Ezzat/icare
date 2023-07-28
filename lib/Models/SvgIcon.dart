9bdc3131f704402552240f30d2a9f85a239db178
Mohamed-B-Ezzat <72664128+Mohamed-B-Ezzat@users.noreply.github.com>
2023-07-28 16:01:34 +0200

    Add files via upload

---
+++ lib/Models/SvgIcon.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/svg.dart';
+
+class SvgIcon {
+
+  late String iconsvg;
+  Widget svg = SvgPicture.asset(
+    assetName,
+  );
+
+  static get assetName => null;
+
+}
